0a83cb48ec915e82e4e694fcc33dad05fff05cdc
Danielvi <danielvilopez@gmail.com>
2025-06-30 00:34:53 -0400

    Add project files.

---
+++ AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ApiClase.Controllers;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace ApiClase;
+
+public class AppDbContext: DbContext
+{
+    public DbSet<Productos> productos => Set<Productos>();
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) 
+        : base(options)
+    {
+
+    }
+       
+
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.DynamoDBv2.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiClase.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    public static List<Productos> listap = new List<Productos>();
+
+
+    [HttpGet("[Action]")]
+    public List<Productos> Get()
+    {
+        return listap;
+    }
+
+    [HttpPost("[Action]")]
+    public void add(Productos producto)
+    {
+       listap.Add(producto);    
+    }
+
+}
+
+public class Productos
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; } = string.Empty;
+
+  
+}
